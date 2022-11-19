d287eacfd06ff739406a97e06f77ea14a3c07ed9
Arsh Khatwani <arsh.khatwani@gmail.com>
2022-11-19 12:17:23 +0530

    basic and custom types

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
 import "./App.css";
 
+/*
+Basic types
+let name: string = "arsh";
+let age: number = 10;
+let flag: boolean = false;
+let hobbies: string[];
+let role: [number, string];
+role = [10, "asd"];
+*/
+
+type Person = {
+  name: string;
+  age: number;
+  gender?: string; // '?' makes it optional
+};
+
+let person: Person;
+person = { name: "ars", age: 10 };
+
+let lotsOfPeople: Person[];
+lotsOfPeople = [
+  { name: "arsh", age: 22, gender: "m" },
+  { name: "sparsh", age: 18 },
+];
+
 function App() {
   return (
     <div className="App">
