7969bbff4a121e4447da03f6ade0494e7c770992
Arsh Khatwani <arsh.khatwani@gmail.com>
2022-11-19 18:46:40 +0530

    Add handle add todo logic

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -4,13 +4,14 @@ import "./styles.css";
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>; //just copy this from hovering cursor over setTodo to view type
+  handleAdd: (e: React.FormEvent) => void;
 }
 
 // const InputField = ({ todo, setTodo }: Props) => {
 // another way to give type to props
-const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
   return (
-    <form className="input">
+    <form className="input" onSubmit={handleAdd}>
       <input
         value={todo}
         type="input"
