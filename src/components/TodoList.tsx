a7a45ba20fd024189d300a767c2f9a73e61961cc
Arsh Khatwani <arsh.khatwani@gmail.com>
2022-11-20 20:24:46 +0530

    Add separate container for completed tasks

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -9,15 +9,22 @@ interface Props {
 
 const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
   return (
-    <div className="todos">
-      {todos.map((item) => (
-        <SingleTodo
-          todo={item}
-          key={item.id}
-          todos={todos}
-          setTodos={setTodos}
-        />
-      ))}
+    <div className="container">
+      <div className="todos">
+        <span className="todos__heading">Active tasks</span>
+        {todos.map((item) => (
+          <SingleTodo
+            todo={item}
+            key={item.id}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
+
+      <div className="todos remove">
+        <span className="todos__heading">Completed tasks</span>
+      </div>
     </div>
   );
 };
