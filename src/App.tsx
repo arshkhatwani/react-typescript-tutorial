1f0e8f2372ea7f4c1fd0841bc95d87dd38b01d6e
Arsh Khatwani <arsh.khatwani@gmail.com>
2022-11-21 09:36:20 +0530

    Add onDragEnd logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import InputField from "./components/InputField";
 import TodoList from "./components/TodoList";
 import { Todo } from "./model";
 import "./components/styles.css";
-import { DragDropContext } from "react-beautiful-dnd";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
@@ -17,8 +17,39 @@ const App: React.FC = () => {
     setTodo("");
   };
 
+  const onDragEnd = (result: DropResult) => {
+    const { source, destination } = result;
+    if (!destination) return;
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return;
+
+    let add,
+      active = todos,
+      complete = completedTodos;
+
+    if (source.droppableId === "TodosList") {
+      add = active[source.index];
+      active.splice(source.index, 1);
+    } else {
+      add = complete[source.index];
+      complete.splice(source.index, 1);
+    }
+
+    if (destination.droppableId === "TodosList") {
+      active.splice(destination.index, 0, add);
+    } else {
+      complete.splice(destination.index, 0, add);
+    }
+
+    setCompletedTodos(complete);
+    setTodos(active);
+  };
+
   return (
-    <DragDropContext onDragEnd={() => {}}>
+    <DragDropContext onDragEnd={onDragEnd}>
       <div className="App">
         <span className="heading">Taskify</span>
 
